2b258fe9adc5c0f07cc3a7781e05605e4b99208b
Nolan Frame <frame.nolan@gmail.com>
2022-08-23 10:19:24 -0700

    saving changes

--- src/components/Shift.js
+++ src/components/Shift.js
@@ -5,7 +5,11 @@ function Shift() {
     return (
         <card className='Shift'>
             <h3>Shift</h3>
-
+            <form>
+                <label>Enter your name: 
+                    <input type="text" name='name' readOnly/>
+                </label>
+            </form>
         </card>
     )
 }
